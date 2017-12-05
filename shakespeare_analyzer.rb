2525d725cf14c202b334a22cde037fe13a5aab36
Aarron Lee <aclee91@gmail.com>
2017-12-04 23:37:12 -0500

    bugfix: put char_name in quotes to handle for spaces, other chars. parsed uniq speaker names from xml

--- shakespeare_analyzer.rb
+++ shakespeare_analyzer.rb
@@ -35,33 +35,39 @@ class PlayInfo
     if !play_xml
       return nil
     end
-    @characters = play_xml.xpath("//PERSONA").map do |character|
-      play_char = PlayCharacter.new(character.inner_text)
+    characters_names = play_xml.search("SPEAKER").map{ |node| node.inner_text }.uniq
+    @characters = []
+    characters_names.each do |character_name|
+      play_char = PlayCharacter.new(character_name)
       play_char.lines = parseCharacterLines(play_xml, play_char)
-      play_char
+      @characters.push(play_char)
     end
   end
 
   def parseCharacterLines(play_xml, play_char)
-    char_name = play_char.name.gsub(/[!@%&, ."]/, '')
-    character_line_nodes = play_xml.search("SPEAKER[text()=#{char_name}] ~ LINE")
+    char_name = play_char.name
+    character_line_nodes = play_xml.search("SPEAKER[text()=\"#{char_name}\"] ~ LINE")
     character_lines = character_line_nodes.map(&:inner_text)
 
     return character_lines ? character_lines : []
+  end
+
+  def characters
+    @characters
   end
 
 end
 
 
 class Api
-  BASE_URL = 'http://www.ibiblio.org/xml/examples/shakespeare/'
 
-  def initialize( play_name='macbeth' )
+  def initialize( play_name='macbeth', base_url=nil )
     @play_name = play_name
+    @base_url = base_url ? base_url : 'http://www.ibiblio.org/xml/examples/shakespeare/'
   end
 
   def getPlayInfo()
-    response = Faraday.get("#{BASE_URL}#{@play_name}.xml")
+    response = Faraday.get("#{@base_url}#{@play_name}.xml")
     @xml_info = Nokogiri::XML.parse(response.body)
     PlayInfo.new( @xml_info )
   end
@@ -73,7 +79,10 @@ api_endpoint = Api.new()
 
 play_info = api_endpoint.getPlayInfo()
 
-
+sorted_characters = play_info.characters.sort_by{|char| char.line_count}.reverse
+sorted_characters.each do |character|
+  puts "#{character.name} - #{character.line_count}"
+end
 
 
 
