8b803cf2616c9788aa87a2517770bf7ca776f382
Aarron Lee <aclee91@gmail.com>
2017-12-05 14:05:01 -0500

    add: form and submit handlers

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -8,20 +8,55 @@ class Root extends React.Component{
     this.state = {
       play_name: 'macbeth',
       play_characters: [],
+      errors: [],
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e){
+
+    this.setState({play_name: e.target.value});
+
   }
 
   handleSubmit(e){
     e.preventDefault();
 
-    
+    let root = this;
+    let handleSuccess = (payload) =>{
+      if(payload && payload.length){
+        root.setState({play_characters: payload, errors: []})
+      }
+    };
+    let handleFailure = (payload) =>{
+      root.setState({errors: [payload], play_characters: []})
+    }
+
+    let queryString = "?play_name=" + this.state.play_name;
+
+    $.ajax({
+        method: 'get',
+        url: `/api/characters${queryString}`
+    }).then(
+      handleSuccess,
+      handleFailure);
 
   }
 
   render(){
 
-    return (<div>React is working!</div>);
+    return (<div>
+      <form onSubmit={this.handleSubmit}>
+        <select value={this.state.play_name} onChange={this.handleChange}>
+          <option value="macbeth">Macbeth</option>
+          <option value="othello">Othello</option>
+          <option value="hamlet">Hamlet</option>
+          <option value="dream">A Midsummer Night's Dream</option>
+        </select>
+        <button>Submit</button>
+      </form>
+    </div>);
 
   }
 
