0a4dbbde7bebed2c65cc22f730c7c4f70e67637d
Aarron Lee <aclee91@gmail.com>
2017-12-05 14:11:58 -0500

    minor reformat of function invocations

--- app/controllers/api/characters_controller.rb
+++ app/controllers/api/characters_controller.rb
@@ -2,20 +2,20 @@ require "api_endpoint"
 class Api::CharactersController < ApplicationController
 
   def index
-    if(params[:play_name])
+    if params[:play_name]
       @play_name = params[:play_name]
     end
     api_endpoint = nil
-    if( @play_name )
-      api_endpoint = ApiEndpoint.new(@play_name)
+    if @play_name
+      api_endpoint = ApiEndpoint.new @play_name
     else
-      api_endpoint = ApiEndpoint.new()
+      api_endpoint = ApiEndpoint.new
     end
-    play_info = api_endpoint.getPlayInfo()
+    play_info = api_endpoint.getPlayInfo
 
     sorted_characters = play_info.characters.sort_by{|char| char.line_count}.reverse
 
-    render json: sorted_characters.map{ |char| {name: char.name, line_count: char.line_count }}
+    render json: sorted_characters.map{ |char| { name: char.name, line_count: char.line_count } }
   end
 
 
